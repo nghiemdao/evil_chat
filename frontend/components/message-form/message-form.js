11e434188692f782157bf38117c6f48e72660783
nghiemdao <nghiemvandao@gmail.com>
2018-01-04 00:06:12 +0700

    Finish chat function

--- frontend/components/message-form/message-form.js
+++ frontend/components/message-form/message-form.js
@@ -1,2 +1,29 @@
 // frontend/components/message-form/message-form.js
+import { sendMessage } from "client/chat";
 import "./message-form.css";
+
+const form = document.querySelector(".js-message-form");
+const input = form.querySelector(".js-message-form--input");
+const submit = form.querySelector(".js-message-form--submit");
+
+function submitForm() {
+  // Invokes sendMessage, that, in turn, invokes Ruby send_message method
+  // that will create a Message instance with ActiveRecord
+  sendMessage(input.value);
+  input.value = "";
+  input.focus();
+}
+
+// You can send a message with cmd/ctrl+enter
+input.addEventListener("keydown", event => {
+  if (event.keyCode === 13 && event.metaKey) {
+    event.preventDefault();
+    submitForm();
+  }
+});
+
+// Or by clicking a button
+submit.addEventListener("click", event => {
+  event.preventDefault();
+  submitForm();
+});
